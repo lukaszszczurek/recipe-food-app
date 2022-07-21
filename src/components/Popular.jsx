3d7c46752e8a631c9c413911dd1f33999bd86c43
Lukasz <lukaszszczurek02@gmial.com>
2022-07-21 10:40:28 +0200

    Popular styled working

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-
+import styled from "styled-components";
 
 function Popular() {
     const [popular,setPopular]=useState([]);
@@ -11,8 +11,9 @@ function Popular() {
  const getPopular=async ()=>{
      const  api= await  fetch("https://api.spoonacular.com/recipes/random?apiKey=ec2df15c2abd45769b40f8c7cad212b5&number=9");
         const data=await api.json();
-        console.log(data)
+
      setPopular(data.recipes);
+     console.log(data.recipes)
  };
 
 
@@ -20,15 +21,35 @@ function Popular() {
         <div>
             {popular.map((r)=>{
                return(
-                   <div key={r.id}>
-                   <p>
-                       {r.title}
-                   </p>
-                   </div>
+                   // <div key={r.id}>
+                   // <p>
+                   //     {r.title}
+                   // </p>
+                   // </div>
+                   <Wrapper>
+                       <h3>Popular</h3>
+                       {popular.map((r)=>{
+                           return(
+                               <Card>
+                                   <p>{r.title}</p>
+                               </Card>
+                           );
+
+                       })}
+                   </Wrapper>
                );
             })}
         </div>
     );
 }
+
+const Wrapper=styled.div`
+margin: 4rem 0rem;
+`;
+
+const Card=styled.div`
+min-height: 25rem;
+  border-radius: 2rem;
+`;
 
 export default Popular;
